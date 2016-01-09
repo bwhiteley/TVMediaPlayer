0aee03e887bd27b946431730443fbb4aabf82170
Bart Whiteley <@bwhiteley>
2016-01-09 01:10:22 -0700

    Initial Commit

---
+++ TVMediaPlayer/TVMediaPlayer.h
@@ -0,0 +1,19 @@
+//
+//  TVMediaPlayer.h
+//  TVMediaPlayer
+//
+//  Created by J. B. Whiteley on 1/9/16.
+//  Copyright © 2016 SwiftBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TVMediaPlayer.
+FOUNDATION_EXPORT double TVMediaPlayerVersionNumber;
+
+//! Project version string for TVMediaPlayer.
+FOUNDATION_EXPORT const unsigned char TVMediaPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TVMediaPlayer/PublicHeader.h>
+
+
