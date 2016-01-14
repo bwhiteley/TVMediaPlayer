3e0fae1a369a636fbed1bc267ce2615fee7ee66a
Bart Whiteley <@bwhiteley>
2016-01-14 00:07:15 -0700

    fix NaN crash

--- TVMediaPlayer/MediaItemType.swift
+++ TVMediaPlayer/MediaItemType.swift
@@ -40,8 +40,10 @@ extension MediaItemType {
         return length * (1.0 - NSTimeInterval(position))
     }
     
-    func timeStringsAtPosition(position:Float) -> (elapsed:String, remaining:String) {
-        let length:Float = Float(self.length)
+    func timeStringsAtPosition(var position:Float) -> (elapsed:String, remaining:String) {
+        if isnan(position) || isinf(position) { position = 0 }
+        var length:Float = Float(self.length)
+        if isnan(length) || isinf(length) { length = 0 }
         let secondsElapsed = length * position
         let secondsRemaining = length * (1.0 - position)
         return (timeIntervalDisplayValue(seconds: Int(secondsElapsed)), timeIntervalDisplayValue(seconds: Int(secondsRemaining)))
